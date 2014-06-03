5966a57663dbaa6a867e7c79d78528734f4452ff
Terence Siganakis <terence@siganakis.com>
2014-06-03 19:25:22 +1000

    Removed old file

--- MSGooroo.SqlBacked/HelperTemplates.cs
+++
@@ -1,7 +0,0 @@
-﻿
-			public static DbParameter GetParameter(DbCommand cmd, string name, object val){
-				DbParameter p = cmd.CreateParameter();
-				p.ParameterName = name;
-				p.Value = val == null ? (object) DBNull.Value : val;
-				return p;
-			}
