7ee936b012ec53359cda84bb4912c2b91e88b17f
Terence Siganakis <terence@siganakis.com>
2015-03-23 14:33:57 +1100

    Moved PerformanceLogger.cs

--- GoorooIO.SqlBacked/code/PerformanceLogger.cs
+++ GoorooIO.SqlBacked/code/PerformanceLogger.cs
@@ -4,23 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web;
+using System.Collections;
+using System.Diagnostics;
 
-namespace MSGooroo.SqlBacked {
+namespace GoorooIO.SqlBacked {
 	public class PerformanceLogger : IDisposable {
 
 		public class Counter {
 			public double TotalMilliseconds;
-			public int Count; 
+			public int Count;
+			public string Stack;
 
-			public void Add(PerformanceLogger log){
+			public void Add(PerformanceLogger log) {
 				Count++;
 				TotalMilliseconds += log._time;
+
+
+				Stack = Environment.StackTrace;
+
 			}
 		}
 
-		public static double TotalTime(string counterName) {
+		public static double TotalTime(string counterName, IDictionary persistence) {
 			Counter counter;
-			var counters = PerformanceCounters;
+			var counters = PerformanceCounters(persistence);
 
 			if (counters != null && counters.TryGetValue(counterName, out counter)) {
 				return counter.TotalMilliseconds;
@@ -28,9 +35,9 @@ namespace MSGooroo.SqlBacked {
 				return 0;
 			}
 		}
-		public static int Count(string counterName) {
+		public static int Count(string counterName, IDictionary persistence) {
 			Counter counter;
-			var counters = PerformanceCounters;
+			var counters = PerformanceCounters(persistence);
 
 			if (counters != null && counters.TryGetValue(counterName, out counter)) {
 				return counter.Count;
@@ -39,28 +46,32 @@ namespace MSGooroo.SqlBacked {
 			}
 		}
 
-		public static Dictionary<string, Counter> PerformanceCounters {
-			get {
-				return HttpContext.Current.Items["request-counters"] as Dictionary<string, Counter>;
+		public static Dictionary<string, Counter> PerformanceCounters(IDictionary persistence) {
+			if (persistence != null) {
+				return persistence["request-counters"] as Dictionary<string, Counter>;
+			} else {
+				return null;
 			}
 		}
-		private static void Increment(PerformanceLogger logger) {
-			if (HttpContext.Current != null) {
-				if (HttpContext.Current.Items != null) {
+		private void Increment(PerformanceLogger logger) {
+			if (_persistence != null) {
+				if (_persistence != null) {
 					Dictionary<string, Counter> counters;
-					if (HttpContext.Current.Items.Contains("request-counters")) {
-						counters = HttpContext.Current.Items["request-counters"] as Dictionary<string, Counter>;
+					if (_persistence.Contains("request-counters")) {
+						counters = _persistence["request-counters"] as Dictionary<string, Counter>;
 					} else {
 						counters = new Dictionary<string, Counter>();
-						HttpContext.Current.Items["request-counters"] = counters;
+						_persistence["request-counters"] = counters;
 					}
 
 					if (counters.ContainsKey(logger._name)) {
 						counters[logger._name].Add(logger);
 					} else {
-						counters[logger._name] = new Counter() { 
-							TotalMilliseconds = logger._time, 
-							Count = 1 };
+						counters[logger._name] = new Counter() {
+							TotalMilliseconds = logger._time,
+							Count = 1,
+							Stack = Environment.StackTrace
+						};
 					}
 				}
 
@@ -72,7 +83,11 @@ namespace MSGooroo.SqlBacked {
 		private DateTime _startTime;
 		private double _time;
 		private string _name;
-		public PerformanceLogger(string name) {
+		private IDictionary _persistence;
+
+
+		public PerformanceLogger(string name, System.Collections.IDictionary persistence) {
+			_persistence = persistence;
 			_startTime = DateTime.UtcNow;
 			_name = name;
 
@@ -81,6 +96,9 @@ namespace MSGooroo.SqlBacked {
 		public void Dispose() {
 			_time = DateTime.UtcNow.Subtract(_startTime).TotalMilliseconds;
 			Increment(this);
+
+
+			System.Diagnostics.Trace.WriteLine(string.Format("log\t{0}: {1}ms", _name, _time));
 		}
 	}
 }
